73a3adcf9b1fe1dcd7c3c516d66245cb507b28cf
sesmet2001 <sesmet2001@gmail.com>
2023-10-28 08:51:24 +0200

    ok

--- 3-update_stockdata.py
+++ 3-update_stockdata.py
@@ -79,6 +79,7 @@ def main():
 
 
     # LOAD TICKER DATA #
+    # test
     # my_ticker_query = """SELECT Ticker FROM _yahoo_fin_tickers WHERE Dow == 1 OR PreciousMetals == 1 OR Crypto == 1 OR Portfolio == 1"""
     my_ticker_query = """SELECT Ticker FROM _yahoo_fin_tickers WHERE Dow == 1 OR Portfolio == 1 AND Ticker != 'ARM' OR Crypto == 1 OR PreciousMetals == 1"""
     cur_info.execute(my_ticker_query)    
