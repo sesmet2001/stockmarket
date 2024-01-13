37aaa2f67d198c6419de459852f1c6a34d113e0d
Stefan Smet <smet.stefan@telenet.be>
2024-01-13 14:46:32 +0100

    ok

--- base/stock.py
+++ base/stock.py
@@ -132,15 +132,15 @@ class Stock(Asset):
                 row=1, col=1
             )
             fig.add_trace(
-                go.Scatter(x=self.plotdata.index,y=self.plotdata['BB_up'],mode='lines',name='BB upper',line_color='grey',opacity=0.3),
+                go.Scatter(x=self.plotdata.index,y=self.plotdata['BB_up'],mode='lines',name='BB upper',line_color='grey',opacity=0.2),
                 row=1, col=1
             )
             fig.add_trace(
-                go.Scatter(x=self.plotdata.index,y=self.plotdata['BB_mid'],mode='lines',name='BB middle',line_color='grey',opacity=0.3),
+                go.Scatter(x=self.plotdata.index,y=self.plotdata['BB_mid'],mode='lines',name='BB middle',line_color='grey',opacity=0.1),
                 row=1, col=1
             )
             fig.add_trace(
-                go.Scatter(x=self.plotdata.index,y=self.plotdata['BB_low'],mode='lines',name='BB lower',line_color='grey',opacity=0.3),
+                go.Scatter(x=self.plotdata.index,y=self.plotdata['BB_low'],mode='lines',name='BB lower',line_color='grey',opacity=0.2),
                 row=1, col=1
             )
             #fig.add_trace(
