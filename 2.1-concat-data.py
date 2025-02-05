b35845a9a95fbd865830b3b9781ec836ae7b99b2
Stefan Smet <smet.stefan@telenet.be>
2025-02-05 20:40:17 +0100

    ok

--- 2.1-concat-data.py
+++ 2.1-concat-data.py
@@ -19,6 +19,7 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+from sqlalchemy import create_engine, Integer, String, Float, DateTime
 
 def main():      
     #sys.stdout = open('log-21-concat-data.txt','w') 
@@ -30,14 +31,15 @@ def main():
 
     # DB CONNECTIONS #
     DB_PATH = os.getenv('DB_PATH')
-    conn = sqlite3.connect(DB_PATH + "/database/stockradar-lite.db")
-    cur = conn.cursor()
+    #conn = sqlite3.connect(DB_PATH + "/database/stockradar-lite.db")
+    engine = create_engine("sqlite:///" + DB_PATH + "/database/stockradar-lite.db", echo=True)
+    #cur = conn.cursor()
 
     # LOAD ticker DATA #
     my_ticker_query = """SELECT * FROM _yahoo_fin_tickers WHERE screener == 1 OR dow == 1 OR sp500 == 1 OR nasdaq == 1 OR beursrally == 1 OR portfolio == 1 OR crypto == 1 OR preciousMetals == 1 OR exchangeRates == 1 OR oil == 1 OR crypto == 1 OR other == 1"""
-    #my_ticker_query = """SELECT * FROM _yahoo_fin_tickers WHERE beursrally == 1"""
+    my_ticker_query = """SELECT * FROM _yahoo_fin_tickers WHERE sp500 == 1"""
     
-    my_tickers = pd.read_sql(my_ticker_query, conn)
+    my_tickers = pd.read_sql(my_ticker_query, con=engine)
         
     my_final_df = pd.DataFrame()
     for index, row in my_tickers.iterrows():
@@ -45,7 +47,7 @@ def main():
             if (row['Ticker'] != "BRK.B" and row['Ticker'] != "BRK.A"):
                 #print(row['Ticker'])
                 my_data_query = "SELECT * from '" + row['Ticker'] + "'"
-                my_data_df = pd.read_sql_query(my_data_query,conn)
+                my_data_df = pd.read_sql_query(my_data_query,con=engine)
                 my_data_df['Ticker'] = row['Ticker']
                 #my_data_df.set_index(["Date","Ticker"],inplace=True)
                 my_final_df = pd.concat([my_final_df,my_data_df],ignore_index=True)
@@ -54,9 +56,56 @@ def main():
         except Exception as e:
             print(e)
             pass
-    my_final_df.to_sql('all_stocks', conn, if_exists='replace', index=True)
-    cur.close()
-    conn.close()
+
+    #my_final_df.index = my_final_df.index.astype(Integer)
+
+    my_final_df['Date'] = my_final_df['Date'].str.strip()
+    my_final_df['Date'] = pd.to_datetime(my_final_df['Date'])
+    print(my_final_df.info())
+    dtype_dict = {
+        #"index": Integer(),
+        "Date": DateTime(),
+        "Open": Float(),
+        "High": Float(),
+        "Low": Float(),
+        "Close": Float(),
+        "Volume": Integer(),
+        "Dividends": Float(),
+        "Stock Splits": Float(),
+        "BB_up": Float(),
+        "BB_mid": Float(),
+        "BB_low": Float(),
+        "SMA10": Float(),
+        "SMA50": Float(),
+        "SMA150": Float(),
+        "SMA200": Float(),
+        "TEMA5": Float(),
+        "TEMA10": Float(),
+        "TEMA20": Float(),
+        "TEMA50": Float(),
+        "OBV": Float(),
+        "RSI": Float(),
+        "MACD": Float(),
+        "MACDSignal": Float(),
+        "MACDHist": Float(),
+        "ClosePercentChange": Float(),
+        "VolumePercentChange": Float(),
+        "SMA50PercentChange": Float(),
+        "prevTEMA5": Float(),
+        "prevTEMA20": Float(),
+        "prevSMA50": Float(),
+        "prevRSI": Float(),
+        "prevMACD": Float(),
+        "prevMACDSignal": Float(),
+        "MACD_slope": Float(),
+        "MACD_sign_change": Float(),
+        "30": Float(),
+        "70": Float(),
+        "Ticker": String(10),
+        "Capital Gains": Float()
+    }
+    my_final_df.to_sql('all_stocks', con=engine, if_exists='replace', index=False,dtype=dtype_dict)
+    engine.dispose()
 
 if __name__ == "__main__":
     main()
