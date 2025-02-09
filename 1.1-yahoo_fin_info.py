78280a15123491f18cfa53473b568b3be04bc415
Stefan Smet <smet.stefan@telenet.be>
2025-02-09 10:15:03 +0100

    ok

--- 1.1-yahoo_fin_info.py
+++ 1.1-yahoo_fin_info.py
@@ -3,8 +3,15 @@ import yfinance as yf
 import timeit
 import os
 import sqlite3
+import time
 
-def get_info(tickers):
+def main():
+    DB_PATH = os.getenv('DB_PATH')
+    conn = sqlite3.connect(DB_PATH + "/database/stockradar-lite.db")
+    cur = conn.cursor()
+    counter = 0
+    print("starting")
+
     column_names = ['sector',
                         'fullTimeEmployees', 
                         'longBusinessSummary',
@@ -51,32 +58,29 @@ def get_info(tickers):
                         'regularMarketPrice'] 
         
     info_df = pd.DataFrame(columns=column_names)
-    for ticker in tickers:
-        print(ticker)
+ 
+    tickers_sql = """SELECT Ticker FROM _yahoo_fin_tickers"""
+    cur.execute(tickers_sql)
+    my_tickers = cur.fetchall()
+    my_tickers = [x[0] for x in my_tickers]
+    for my_ticker in my_tickers:
+        print(str(counter) + ": " + my_ticker)
         try:
-            my_ticker = yf.Ticker(ticker)
-            my_ticker_info = my_ticker.info
+            my_ticker_obj = yf.Ticker(my_ticker)
+            my_ticker_info = my_ticker_obj.info
             my_ticker_info_filtered = { key:value for key, value in my_ticker_info.items() if key in column_names }
             my_ticker_info_filtered_df = pd.DataFrame(my_ticker_info_filtered,index=[0])
-            my_ticker_info_filtered_df['Ticker'] = ticker
+            my_ticker_info_filtered_df['Ticker'] = my_ticker
             info_df = pd.concat([info_df,my_ticker_info_filtered_df],ignore_index=True)
         except Exception as e:
             print(e)
             pass
-    return info_df
-
-def main():
-
-    DB_PATH = os.getenv('DB_PATH')
-    conn = sqlite3.connect(DB_PATH + "/database/stockradar-lite.db")
-    cur = conn.cursor()
- 
-    my_tickers = """SELECT Ticker FROM _yahoo_fin_tickers"""
-    cur.execute(my_tickers)
-    tickers = cur.fetchall()
-    tickers = [x[0] for x in tickers]
-    get_info(tickers).to_sql('_yahoo_fin_tickers', conn, if_exists='replace')  
-    #
+        counter = counter + 1
+        if counter % 100 == 0:
+            print("Going to sleep 10 sec.")
+            time.sleep(10)
+    print(info_df)
+    return info_df.to_sql('_yahoo_fin_info', conn, if_exists='replace')  
 
     # # revenue info
     # my_tickers = """SELECT Symbol FROM _yf_financials_revenue_scores WHERE `Total Revenue` > 1000000 ORDER BY RevenuePctChange DESC LIMIT 100"""
@@ -110,4 +114,4 @@ def main():
     conn.close()
 
 if __name__ == "__main__":
-    print(timeit.timeit(main,number=1))
+    main()
